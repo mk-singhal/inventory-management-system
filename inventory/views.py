467a0d4c4959b8cb28a4cd6737c9592ad26b8539
mk-singhal <maniksingal29@gmail.com>
2023-06-01 22:51:50 +0530

    Added Search and paginator in Invetory section

--- inventory/views.py
+++ inventory/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, redirect
 from .models import Product
+from django.core.paginator import Paginator
+from django.db.models import Q  # New
+from django.db.models.functions import Lower
+
 
 # Create your views here.
 def inventory(request):
-    product = Product.objects.all()
-    return render(request, 'inventory/inventory.html', {'sb':2, 'products':product})
+    # For searching the products
+    search_product = request.GET.get('search_product')
+    if search_product:
+        product = Product.objects.filter(Q(name__icontains=search_product) | Q(hsn_code__icontains=search_product))
+    else:
+        # If not searched, return default posts
+        product = Product.objects.all().order_by(Lower("name"))
+    
+    p = Paginator(product, 12)  # creating a paginator object
+    # getting the desired page number from url
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
+    # context = {'page_obj': page_obj}
+    return render(request, 'inventory/inventory.html', {'sb':2, 'page_obj': page_obj})
 
 
 def add_product(request):
     if request.method == 'POST':
-        print("\n\n##################", request.POST, request.FILES['img'])
+        # print("\n\n##################", request.POST, request.FILES['img'])
         instance = Product()
         instance.name = request.POST['name']
         instance.pic = request.FILES['img']
@@ -39,9 +62,9 @@ def delete_product(request, product_id):
 def edit_product(request, product_id):
     product_instance = Product.objects.get(pk=product_id)
     if request.method == 'POST':
-        print("\n\n##################", request.POST, "\n$$$$$$$$", type(request.POST['mr']))
+        # print("\n\n##################", request.POST, "\n$$$$$$$$", bool(request.FILES.get('img', False)))
         product_instance.name = request.POST['name']
-        if bool(request.FILES.get('filepath', False)) == True:
+        if bool(request.FILES.get('img', False)) == True:
             product_instance.pic = request.FILES['img']
         product_instance.hsn_code = request.POST['hsn']
         product_instance.qty_actual = request.POST['actual_qty']
